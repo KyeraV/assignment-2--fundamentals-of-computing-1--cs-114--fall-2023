45ceaf8077f674aa28312f8576d7dc203c6db6a9
Kyera Vassell <143655706+KyeraV@users.noreply.github.com>
2023-10-14 20:23:20 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -1,7 +1,30 @@
 public class Diamond {
   public static void main(String[] args) {
-    Scanner input = new Scanner (System.in);
-    System.out.println("enter a number")
 
+    int j, y, char;
+
+    Scanner input = new Scanner (System.in);
+    System.out.println("enter a number");
+
+    for(j = 0; j <=char; j++) {
+      for(y = 1; y <= char - j; y++) {
+        System.out.print("");
+      }
+      for(y = 1; y <= 2*j-1; Y++) {
+        System.out.print("x");
+      }
+      System.out.printlin("");
+    }
+
+    for(j = char - 1; j >= 1; j--) {
+      for(y = 1; y <= char - j; y++) {
+        System.out.print("");
+      }
+      for(y = 1; y <= 2*j-1; y++) {
+        Sytem.out.print(" *");
+      }
+      System.out.println("")
+    }
   }
+
 }
