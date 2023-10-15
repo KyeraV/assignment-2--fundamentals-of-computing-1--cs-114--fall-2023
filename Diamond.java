d997cf6078b54273cd7e4ed380a3e95cfaeaec16
Kyera Vassell <143655706+KyeraV@users.noreply.github.com>
2023-10-15 17:22:46 -0400

    edit

--- Diamond.java
+++ Diamond.java
@@ -1,30 +1,14 @@
+/*Prints a diamond shape using asterisk (star) characters */
+
 public class Diamond {
   public static void main(String[] args) {
 
-    int j, y, char;
-
+    int row = input.nextInt();
+    int y, b;
     Scanner input = new Scanner (System.in);
     System.out.println("enter a number");
 
-    for(j = 0; j <=char; j++) {
-      for(y = 1; y <= char - j; y++) {
-        System.out.print("");
-      }
-      for(y = 1; y <= 2*j-1; Y++) {
-        System.out.print("x");
-      }
-      System.out.printlin("");
-    }
-
-    for(j = char - 1; j >= 1; j--) {
-      for(y = 1; y <= char - j; y++) {
-        System.out.print("");
-      }
-      for(y = 1; y <= 2*j-1; y++) {
-        Sytem.out.print(" *");
-      }
-      System.out.println("")
-    }
+    for (int )
   }
 
 }
