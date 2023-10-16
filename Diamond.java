f0957d4145cfd974a8a13584f46ce5bad6d764ae
Kyera Vassell <143655706+KyeraV@users.noreply.github.com>
2023-10-16 01:32:21 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -10,48 +10,40 @@ public class Diamond {
     int b, y, r;
     int space = rows/2;
 
-    if(rows %2 == 0) {
-      for(b = 1; b <= space; b++) {
-        for(y = 1; y <= space - b + 1; y++) {
+    if(rows % 2 == 0) {
+      for(b = 1; b <= space; b++) { //top half (even numbers)
+        for(y = 1; y <= space - 1; y++) { // whitespaces
           System.out.print(" ");
         }
-        for( r = 1; r <= (b*2) - 1; r++) {
-          if(r == 1|| r == 2*b-1) {
-             System.out.print("* ");
-          } else {
-            System.out.print(" ");
+        for( r = 1; r <= (b * 2 - 1 ); r++) { //print asterisks (stars)
+          System.out.print(" *");
           }
-        }
-        System.out.println();
+          System.out.println();
       }
-      for(b = space; b >= 1; b--) {
-        for(y = 1; y <= space - b + 1; y++) {
+      for(b = space; b >= 1; b--) { //bottom half (even numbers)
+        for(y = 1; y <= space - b; y++) { // whitespaces
           System.out.print(" ");
         }
-        for(r = 1; r <= (b*2); r++) {
-          if(r == 1 || r == 2*b-1) {
-            System.out.print("* ");
-          } else {
-            System.out.print(" ");
-          }
+        for(r = 1; r <= (b * 2 - 1); r++) { //print asterisks (stars)
+          System.out.print(" *");
         }
         System.out.println();
       }
     } else{
-      for(b = 1; b <= size; b++) { // top half
-        for(y = 1; y <= (size-b); y++) { // whitespaces
+      for(b = 1; b <= size; b++) { // top half (odd numbers)
+        for(y = 1; y <= (size - b); y++) { // whitespaces
           System.out.print(" ");
         }
-        for(r = 1; r <= (b*2-1); r++) { //print asterisks (stars)
+        for(r = 1; r <= (b * 2 - 1); r++) { //print asterisks (stars)
           System.out.print("*");
         }
        System.out.println();
       }
-      for(b = 1; b <= (size-1); b++) { // bottom half
-        for(y = 1; y <=b; y++) { // whitespaces
+      for(b = 1; b <= (size - 1); b++) { // bottom half (odd numbers)
+        for(y = 1; y <= b; y++) { // whitespaces
         System.out.print(" ");
         }
-        for(r = 1; r <= 2*(size-b)-1; r++) { // Print stersisks (stars)
+        for(r = 1; r <= 2* (size - b) - 1; r++) { // //print asterisks (stars)
           System.out.print("*");
         }
        System.out.println();
