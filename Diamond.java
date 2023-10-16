beb5421e72aefd7a32f2fc5074c5643797844e41
Kyera Vassell <143655706+KyeraV@users.noreply.github.com>
2023-10-16 00:09:25 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -1,38 +1,40 @@
 import java.util.Scanner;
-/*Prints a diamond shaoe using As */
+/*Prints a diamond shape using asterisks (stars) */
 
 public class Diamond {
   public static void main(String[] args) {
     Scanner input = new Scanner (System.in);
     System.out.print("enter a number");
-    int row = input.nextInt();
-    int number = input.nextInt();
-    int y, b;
+    int rows = input.nextInt();
+    int size = (rows+1)/2;
+    int b, y, r;
+    int space = rows/2;
 
-    if(number%2==0) {
-      System.out.println("even number");
-    }else{
-      System.out.println("odd number");
-    }
-
-    for(y = 0; y <=row; y++) {
-      for(b = 1; b <= row - y; b++) {
+    if(row s %2==0) {
+      for(b = 1; b <= rows / 2; b++) {
+        for(y = 1; y <= rows / 2 - 1; y++) {
+          System.out.print(" ")
+        }
+      }
+    }else {
+      for(b = 1; b <= size; b++) { // top half
+        for(y = 1; y <= (size-b); y++) { // whitespaces
+          System.out.print(" ");
+        }
+        for(r = 1; r <= (b*2-1); r++) { //print asterisks (stars)
+          System.out.print("*");
+        }
+       System.out.println();
+      }
+      for(b = 1; b <= (size-1); b++) { // bottom half
+        for(y = 1; y <=b; y++) { // whitespaces
         System.out.print(" ");
+        }
+        for(r = 1; r <= 2*(size-b)-1; r++) { // Print stersisks (stars)
+          System.out.print("*");
+        }
+       System.out.println();
       }
-      for(b = 1; b <= 2*y-1; b++) {
-        System.out.print("*");
-      }
-      System.out.println("");
-    }
-    for(y = row - 1; y >= 1; y--) {
-      for(b = 1; b <= row - y; b++) {
-        System.out.print(" ");
-      }
-      for(b = 1; b <= 2*y-1; b++) {
-        System.out.print(" *");
-      }
-      System.out.println(" *");
     }
   }
-
 }
